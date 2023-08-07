b9e3ed7ed962be008b318376733a79d548866844
Chantal Pic <chantal.pic@ec.gc.ca>
2023-08-07 18:19:19 -0400

    Add a script to download SPS database

--- download-dbase.sh
+++ download-dbase.sh
@@ -2,9 +2,11 @@
 
 set -e
 
+
 SPS_DBASE="sps_dbase-6.3.tar.gz"
 SPS_DBASE_URL="https://hpfx.collab.science.gc.ca/~sixf000/SPS/${SPS_DBASE}"
 SPS_DBASE_MD5SUM="bcaea70b593aa147db0ab87132a14334"
+
 
 printUsage() {
     echo -e "Download a sample database of data files needed to run SPS"
