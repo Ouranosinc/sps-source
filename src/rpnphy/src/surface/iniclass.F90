cc67ed5a924f519a2178fdb9cb4dfdf87fe338d3
Charles Gauthier <cgauthier@scylla.ouranos.ca>
2025-10-16 08:27:27 -0400

    Pas de path hardcoded pour CLASS_input_table

--- src/rpnphy/src/surface/iniclass.F90
+++ src/rpnphy/src/surface/iniclass.F90
@@ -4,7 +4,7 @@ subroutine iniclass
   !@Author K. Winger
   !@Revisions
   ! 001 K. Winger (UQAM/ESCER) Jun 2020 - Initial version
-
+  ! 002 C. Gauthier (Ouranos)  Oct 2020 - Adapt paths for SPS file structure
 
   use sfc_options  , only : delt
   use sfclayer , only : set_class_const
@@ -14,15 +14,33 @@ subroutine iniclass
 !  character(len=*), intent(in) :: F_path
 !  integer, intent(in) :: F_myproc
 !  integer :: F_istat
+  character(len=255) :: cwd_path, base_path, table_p
+  integer :: ierror, pos
   logical, save :: first = .true.
 
 if (first) then
   first = .false.
   print *,'In iniclass.F90'
-  print *,'iniclass HELLOOOOOOOOOOOOOOOOOOOOOOOOOOO AAA'
 endif
 
-  runParamsFile = "/exec/cgauthier/sps-source/workdir/RUNMOD/input/cfg_0000/CLASS_input_table"
+! Get path to CLASS_input_table following SPS file structure
+  call GETCWD(cwd_path, ierror)
+
+  if (ierror == 0) then
+    cwd_path = trim(cwd_path)
+
+    pos = index(cwd_path, '/RUNMOD/')
+
+    if (pos > 0) then
+      base_path = cwd_path(1:pos + 7)
+      table_p = trim(base_path) // 'input/cfg_0000/CLASS_input_table'
+      print *, "Base path: ", trim(base_path)
+      print *, "New path (table_p): ", trim(table_p)
+    end if
+  else
+    print *, "Error getting current working directory. Error code:", ierror
+  end if
+  runParamsFile = table_p
 
   ! Pass some constants from GEM to CLASS
   call set_class_const(delt)
