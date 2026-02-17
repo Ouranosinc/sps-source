37042571f8d5afbf13d64b31873159428d6b752e
Charles Gauthier <gauthier.charles@ouranos.ca>
2026-02-17 07:55:30 -0500

    Ajout dp_class

--- src/rpnphy/src/surface/sfc_options.F90
+++ src/rpnphy/src/surface/sfc_options.F90
@@ -100,6 +100,10 @@ module sfc_options
    real :: dp_svs(MAX_NL_SVS) = -1.0
    namelist /surface_cfgs/ dp_svs
 
+   !# Depth of soil layers in [METERS] in CLASS land surface scheme (schmsol=CLASS)
+   real :: dp_class(MAX_NL_SVS) = -1.0
+   namelist /surface_cfgs/ dp_class
+
    !# Emissivity for ice (glacier and sea ice)
    !# * '_constant_' : A fixed floating point value used as a constant
    character(len=16) :: ice_emiss = '0.99'
